bcba34330b9189e9999185e670b8dfc32fd59f9c
Yu-Chia Chen <yuchaz@fb.com>
2021-11-14 23:27:32 -0800

    Copied from pypi

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='taming-transformers-rom1504',
+    version='0.0.6',
+    description='Taming Transformers for High-Resolution Image Synthesis',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'torchvision',
+        'numpy',
+        'tqdm',
+        'omegaconf>=2.0.0',
+        'pytorch-lightning>=1.0.8'
+    ],
+)
